946b3531c2d68803bb7e8775e739bc4e04ac0c9e
justoneprimate <jonbchat@gmail.com>
2019-01-01 01:41:08 -0500

    Basic Dice Roller Functions

---
+++ dice_roller.py
@@ -0,0 +1,37 @@
+def d4():
+    import random
+    highest = 4
+    roll = random.randint(1, highest)
+    print(roll)
+
+
+def d6():
+    import random
+    highest = 6
+    roll = random.randint(1, highest)
+    print(roll)
+
+
+def d10():
+    import random
+    highest = 10
+    roll = random.randint(1, highest)
+    print(roll)
+
+
+def d12():
+    import random
+    highest = 12
+    roll = random.randint(1, highest)
+    print(roll)
+
+
+def d20():
+    import random
+    highest = 20
+    roll = random.randint(1, highest)
+    print(roll)
+
+
+d20()
+
