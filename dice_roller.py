0b320592e6d76dbc7c55bf3e8d81ad45743a1b26
jonathan <jonbchat@gmail.com>
2019-01-03 17:53:06 -0500

    Dice Roller Beta

--- dice_roller.py
+++ dice_roller.py
@@ -1,109 +1,33 @@
-# trying to decide how best to use functions and random to roll dice sets
-# the idea is to ask the user how many of each sided dice they want to roll
-# and then print out that roll into a list
+import random
+
+dice_set = {
+    'd4': lambda: random.randint(1, 4),
+    'd6': lambda: random.randint(1, 6),
+    'd8': lambda: random.randint(1, 8),
+    'd10': lambda: random.randint(1, 10),
+    'd12': lambda: random.randint(1, 12),
+    'd20': lambda: random.randint(1, 20),
+    'd100': lambda: random.randint(1, 100),
+}
+
+roll_tally = list()
 
 
-def d4():
-    import random
-    roll = random.randint(1, 4)
-    return roll
+print("Welcome to the dice roller. You can roll d4, d6, d8, d10, d12, d20 or d100. \n"
+      "You may roll up to 6 times per set.")
 
+while len(roll_tally) <= 10:
+    choose_dice = input("Please choose a die to roll or type quit: ")
+    if choose_dice == 'quit':
+        break
+    if choose_dice in dice_set:
+        rolled_dice = dice_set.get(choose_dice)()
+        roll_tally.append((str(choose_dice) + ':' + str(rolled_dice)))
+        for roll in roll_tally:
+            print(roll)
+    else:
+        print("Choose a valid die")
 
-def d6():
-    import random
-    roll = random.randint(1, 6)
-    return roll
-
-
-def d10():
-    import random
-    roll = random.randint(1, 10)
-    return roll
-
-
-def d12():
-    import random
-    roll = random.randint(1, 12)
-    return roll
-
-
-def d20():
-    import random
-    roll = random.randint(1, 20)
-    return roll
-
-
-def d100():
-    import random
-    roll = random.randint(1, 100)
-    return roll
-
-# example of a dictionary for dice
-#
-
-# import random
-#
-# diceDict = {'d4': random.randint(1, 4), 'd6': random.randint(1, 6), 'd10': random.randint(1, 10),
-#             'd12': random.randint(1, 12), 'd20': random.randint(1, 20), 'd100': random.randint(1, 100)}
-
-
-# * d4 = random.randint(1, 4)    #
-# * d6 = random.randint(1, 6)    #
-# * d10 = random.randint(1, 10)  #
-# * d12 = random.randint(1, 12)  #
-# * d20 = random.randint(1, 20)  #
-# * d100 = random.randint(1, 100)#
-
-
-roll_list = list()
-
-dice_choice = input("Which dice would you like to roll?: ")
-
-
-if dice_choice == 'd6':
-    while len(roll_list) <= 5:
-        if input("Press enter to roll your dice, or q to quit: ") == 'q':
-            print(roll_list)
-            break
-        else:
-            roll_list.append(d6())
-            print(roll_list)
-    print("All finished. Your roll set is:")
-    print(roll_list)
-
-# if dice_choice == 'd4':
-#     print(d4())
-# if dice_choice == 'd6':
-#     print(d6())
-# if dice_choice == 'd10':
-#     print(d10())
-# if dice_choice == 'd12':
-#     print(d12())
-# if dice_choice == 'd20':
-#     print(d20())
-# if dice_choice == 'd100':
-#     print(d100())
-
-
-# roll_list = list()
-# roll_list.append(d100())
-# print(roll_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("All finished. Your roll set is:")
+for roll in roll_tally:
+    print(roll)
